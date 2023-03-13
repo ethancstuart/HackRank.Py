cf470c4d7c07118038d6033cb31932c0d228b763
ethancstuart <40368395+ethancstuart@users.noreply.github.com>
2023-03-12 19:34:54 -0700

    Add files via upload

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    # Calculation of the sum, difference and product of the two numbers
+    sum = a + b
+    diff = a - b
+    prod = a * b
+
+    # Printing the results as per the requirement
+    print(sum)
+    print(diff)
+    print(prod)
